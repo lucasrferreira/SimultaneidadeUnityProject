d3f6dc3b7a8e46d04ba79df83ba5f88199fe7791
Lucas Ribeiro Ferreira <ribeiroferreiralucas@gmail.com>
2018-04-12 01:09:33 -0300

    add toogle to control current situation

---
+++ Assets/Scripts/EnableDisableGameObjectWithToggle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace TempoSuspeito
+{
+    public class EnableDisableGameObjectWithToggle : MonoBehaviour
+    {
+        public Toggle Toggle;
+
+        private void Start()
+        {
+            gameObject.SetActive(Toggle.isOn);            
+            Toggle.onValueChanged.AddListener(OnValueChange);
+
+        }
+
+        private void OnDestroy()
+        {
+            Toggle.onValueChanged.RemoveListener(OnValueChange);
+        }
+
+        private void OnValueChange(bool newValue)
+        {
+            gameObject.SetActive(newValue);
+        }    
+    }
+}
---
+++ Assets/Scripts/SaveToggleValue.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TempoSuspeito
+{
+    public class SaveToggleValue : MonoBehaviour
+    {
+        private static readonly Dictionary<string, bool> ValueDic = new Dictionary<string, bool>(); 
+        private Toggle _toggle;
+        public string Context;
+
+        private void Awake()
+        {
+            _toggle = GetComponent<Toggle>();
+        }
+
+        private void Start()
+        {
+            if (ValueDic.ContainsKey(Context))
+            {
+                _toggle.isOn = ValueDic[Context];
+            }
+            else
+            {
+                ValueDic.Add(Context,_toggle.isOn);
+            }
+            
+        }
+
+        private void OnEnable()
+        {
+            _toggle.onValueChanged.AddListener(OnValueChange);
+        }
+
+        private void OnDisable()
+        {
+            _toggle.onValueChanged.RemoveListener(OnValueChange);
+        }
+
+        private void OnValueChange(bool newValue)
+        {
+            ValueDic[Context] = newValue;
+        }    
+    }
+}
